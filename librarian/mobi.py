a4d6831b455ed69b196905427600d7163cdd3fe8
Robert Błaut <listy@blaut.biz>
2014-04-01 13:00:25 +0200

    [mobi] pep8

--- librarian/mobi.py
+++ librarian/mobi.py
@@ -3,6 +3,7 @@
 # This file is part of Librarian, licensed under GNU Affero GPLv3 or later.
 # Copyright © Fundacja Nowoczesna Polska. See NOTICE for more information.
 #
+
 from copy import deepcopy
 import os
 import subprocess
@@ -13,7 +14,7 @@ from librarian.cover import DefaultEbookCover
 from librarian import get_resource
 
 
-def transform(wldoc, verbose=False, sample=None, cover=None, 
+def transform(wldoc, verbose=False, sample=None, cover=None,
               use_kindlegen=False, flags=None):
     """ produces a MOBI file
 
@@ -30,7 +31,7 @@ def transform(wldoc, verbose=False, sample=None, cover=None,
     if not flags:
         flags = []
     flags = list(flags)
-    
+
     epub = document.as_epub(verbose=verbose, sample=sample,
                             html_toc=True, cover=True, flags=flags)
     if verbose:
@@ -39,20 +40,17 @@ def transform(wldoc, verbose=False, sample=None, cover=None,
         devnull = open("/dev/null", 'w')
         kwargs = {"stdout": devnull, "stderr": devnull}
 
-    output_file = NamedTemporaryFile(prefix='librarian', suffix='.mobi', delete=False)
+    output_file = NamedTemporaryFile(prefix='librarian', suffix='.mobi',
+                                     delete=False)
     output_file.close()
 
     if use_kindlegen:
         output_file_basename = os.path.basename(output_file.name)
-        subprocess.check_call(
-            ['kindlegen', '-c2', epub.get_filename(), '-o', output_file_basename],
-            **kwargs
-        )
+        subprocess.check_call(['kindlegen', '-c2', epub.get_filename(),
+                              '-o', output_file_basename], **kwargs)
     else:
-        subprocess.check_call(
-            ['ebook-convert', epub.get_filename(), output_file.name,
-            '--no-inline-toc', 
-            '--mobi-file-type=both', 
-            '--mobi-ignore-margins'], **kwargs
-        )
+        subprocess.check_call(['ebook-convert', epub.get_filename(),
+                               output_file.name, '--no-inline-toc',
+                               '--mobi-file-type=both',
+                               '--mobi-ignore-margins'], **kwargs)
     return OutputFile.from_filename(output_file.name)
