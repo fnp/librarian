ae38fc513217c9ef8d6d92111679246572b84b4a
Jan Szejko <jan.szejko@gmail.com>
2016-03-29 16:55:15 +0200

    fix for generating ebooks without pictures

--- librarian/epub.py
+++ librarian/epub.py
@@ -528,12 +528,13 @@ def transform(wldoc, verbose=False, style=None, html_toc=False,
 
     functions.reg_mathml_epub(zip)
 
-    for i, filename in enumerate(os.listdir(ilustr_path)):
-        file_path = os.path.join(ilustr_path, filename)
-        zip.write(file_path, os.path.join('OPS', filename))
-        image_id = 'image%s' % i
-        manifest.append(etree.fromstring(
-            '<item id="%s" href="%s" media-type="%s" />' % (image_id, filename, guess_type(file_path)[0])))
+    if os.path.isdir(ilustr_path):
+        for i, filename in enumerate(os.listdir(ilustr_path)):
+            file_path = os.path.join(ilustr_path, filename)
+            zip.write(file_path, os.path.join('OPS', filename))
+            image_id = 'image%s' % i
+            manifest.append(etree.fromstring(
+                '<item id="%s" href="%s" media-type="%s" />' % (image_id, filename, guess_type(file_path)[0])))
 
     # write static elements
     mime = zipfile.ZipInfo()
