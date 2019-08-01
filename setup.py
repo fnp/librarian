a1eee7c004e2068b705bb4475633f76f2e38b4c4
Radek Czajka <rczajka@rczajka.pl>
2019-08-01 10:52:51 +0200

    Packaging fix.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def whole_tree(prefix, path):
 
 setup(
     name='librarian',
-    version='1.7.3',
+    version='1.7.4',
     description='Converter from WolneLektury.pl XML-based language to XHTML, TXT and other formats',
     author="Marek Stępniowski",
     author_email='marek@stepniowski.com',
