bfd8ac40bed4535a91bfd645cbe80b47c827a8de
Radek Czajka <rczajka@rczajka.pl>
2020-05-28 17:10:43 +0200

    Fix XML entities left from MathML.

--- librarian/embeds/mathml.py
+++ librarian/embeds/mathml.py
@@ -10,6 +10,17 @@ from . import TreeEmbed, create_embed, downgrades_to
 class MathML(TreeEmbed):
     @downgrades_to('application/x-latex')
     def to_latex(self):
+        """
+        >>> print(MathML(etree.fromstring('<mat>a &lt; b</mat>')).to_latex().data.strip())
+        a < b
+
+        >>> print(MathML(etree.fromstring('<mat>&lt; &amp; &amp;lt; &#65;</mat>')).to_latex().data.strip())
+        < & &lt; A
+
+        """
         xslt = etree.parse(get_resource('res/embeds/mathml/mathml2latex.xslt'))
         output = self.tree.xslt(xslt)
-        return create_embed('application/x-latex', data=six.text_type(output))
+        text = six.text_type(output)
+        # Workaround for entities being preserved in output. But there should be a better way.
+        text = text.replace('&lt;', '<').replace('&amp;', '&')
+        return create_embed('application/x-latex', data=text)
